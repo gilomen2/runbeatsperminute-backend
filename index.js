bc4f979c86af9d9673f264925187746493e39814
Beth Gilomen <beth.gilomen@favordelivery.com>
2019-07-16 20:12:20 -0500

    setup app

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const cors = require('cors')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+let app = express()
+
+const whitelist = ['http://runbeatsperminute.com', 'localhost:3000']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.options('*', cors(corsOptions))
+
+app.get('/test-route', cors(corsOptions), (req, res) => {
+  res.json({
+    text: 'This was a test'
+  })
+})
